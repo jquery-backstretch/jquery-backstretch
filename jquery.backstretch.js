e9adaa0b23b01753750af7ab8ccff8dce467526f
Scott Robbin <scott@srobbin.com>
2010-07-27 22:37:22 -0500

    Updated to version 1.1
    * Added 'centered' option.
    * Removed 'hideUntilReady' option. It looks pretty bad if you don't hide the image until it's fully loaded.
    * Hack to stretch image on iOS, where position:fixed is wonky.

--- jquery.backstretch.js
+++ jquery.backstretch.js
@@ -1,77 +1,82 @@
 /*
- * jQuery Backstretch 1.0
+ * jQuery Backstretch
+ * Version 1.1
  * http://srobbin.com/jquery-plugins/jquery-backstretch/
  *
  * Add a dynamically-resized background image to the page
  *
- * Copyright (c) 2009 Scott Robbin (srobbin.com)
+ * Copyright (c) 2010 Scott Robbin (srobbin.com)
  * Dual licensed under the MIT and GPL licenses.
 */
 
 (function($) {
 
-  $.backstretch = function(src, options, callback) {
-    var settings = {
-          hideUntilReady: true, // Hide the image until it's finished loading
-          speed: 0 // fadeIn speed for background after image loads (e.g. "fast" or 500)
+    $.backstretch = function(src, options, callback) {
+        var settings = {
+            centered: true,         // Should we center the image or leave it fixed at 0,0?
+            speed: 0                // fadeIn speed for background after image loads (e.g. "fast" or 500)
         },
-        imgRatio;
-    
-    // Extend the settings with those the user has provided
-    if(options && typeof options == "object") $.extend(settings, options);
-    
-    // Initialize
-    $(document).ready(_init);
-  
-    // For chaining
-    return this;
-    
-    function _init() {
-      // Prepend image, wrapped in a DIV, with some positioning and zIndex voodoo
-      if(src) {
-        var commonCSS = {left: 0, top: 0},
-            wrap = $("<div />").attr("id", "backstretch-wrap")
-                               .css( $.extend(commonCSS, {position: "absolute", zIndex: -1}) ),
-            container = $("<div />").attr("id", "backstretch")
-                                    .css( $.extend(commonCSS, {position: "fixed", overflow: "hidden", zIndex: -1}) )
-                                    .appendTo(wrap),
-            img = $("<img />").attr("src", src)
-                              .bind("load", function() {
-                                    var self = $(this);
-                                    imgRatio = self.width() / self.height();
-                                    _adjustBG(function() {
-                                      if( settings.hideUntilReady )
-                                        self.fadeIn(settings.speed, function(){
-                                          // Callback, if necessary
-                                          if(typeof callback == "function") callback();
-                                        });
-                                    });
-                                  });
+        rootElement = ("onorientationchange" in window) ? $(document) : $(window), // hack to acccount for iOS position:fixed shortcomings
+        imgRatio, bgImg, bgWidth, bgHeight, bgOffset, bgOffsetCSS;
         
-        if(settings.hideUntilReady) img.hide();          
-        img.appendTo(container);
-          
-        $("body").prepend(wrap);
-
-        // Adjust the background size when the window is resized
-        $(window).resize(_adjustBG);
-      }
-      
-    }
+        // Extend the settings with those the user has provided
+        if(options && typeof options == "object") $.extend(settings, options);
     
-    function _adjustBG(callback) {
-      var bgWidth = $(window).width(),
-          bgHeight = bgWidth / imgRatio;
+        // Initialize
+        $(document).ready(_init);
+  
+        // For chaining
+        return this;
+    
+        function _init() {
+            // Prepend image, wrapped in a DIV, with some positioning and zIndex voodoo
+            if(src) {
+                var commonCSS = {left: 0, top: 0},
+                    container = $("<div />").attr("id", "backstretch")
+                                            .css( $.extend(commonCSS, {position: "fixed", overflow: "hidden", zIndex: -1}) ),
+                    img = $("<img />").attr("src", src)
+                                      .css({position: "relative", display: "none"})
+                                      .bind("load", function() {                                          
+                                          var self = $(this);
+                                              imgRatio = self.width() / self.height();
+                                              
+                                          _adjustBG(function() {
+                                              self.fadeIn(settings.speed, function(){
+                                                  if(typeof callback == "function") callback();
+                                              });
+                                          });
+                                      })
+                                      .appendTo(container);
+                  
+                $("body").prepend(container);
 
-      if(bgHeight < $(window).height()) {
-        bgHeight = $(window).height();
-        bgWidth = bgHeight * imgRatio;
-      }
-
-      $("#backstretch img").width( bgWidth ).height( bgHeight );
+                // Adjust the background size when the window is resized or orientation has changed (iOS)
+                $(window).resize(_adjustBG);
+            }
+        }
+    
+        function _adjustBG(fn) {
+            bgWidth = rootElement.width();
+            bgHeight = bgWidth / imgRatio;
+            
+            // Make adjustments based on image ratio
+            // Note: Offset code provided by Peter Baker (http://ptrbkr.com/). Thanks, Peter!
+            if(bgHeight >= rootElement.height()) {
+                bgOffset = (bgHeight - rootElement.height()) /2;
+                bgOffsetCSS = {left: "0px", top: "-" + bgOffset + "px"};
+            } else {
+                bgHeight = rootElement.height();
+                bgWidth = bgHeight * imgRatio;
+                bgOffset = (bgWidth - rootElement.width()) / 2;
+                bgOffsetCSS = {left: "-" + bgOffset + "px", top: "0px"};
+            }    
       
-      if (typeof callback == "function") callback();
-    }
-  };
+            $("#backstretch img").width( bgWidth ).height( bgHeight );
+            if(settings.centered) $("#backstretch img").css(bgOffsetCSS);
+      
+            // Executed the passed in function, if necessary
+            if (typeof fn == "function") fn();
+        }
+    };
   
 })(jQuery);
