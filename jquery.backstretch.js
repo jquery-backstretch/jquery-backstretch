b72e7ca09ead7a906a242b43901df2f2d27f227d
David Casey <dcasey@drakecooper.com>
2012-11-06 15:20:14 -0700

    added the ability to append markup inside backstretch

--- jquery.backstretch.js
+++ jquery.backstretch.js
@@ -21,39 +21,40 @@
         settings = container.data("settings") || defaultSettings, // If this has been called once before, use the old settings as the default
         existingSettings = container.data('settings'),
         rootElement = ("onorientationchange" in window) ? $(document) : $(window), // hack to acccount for iOS position:fixed shortcomings
-        imgRatio, bgImg, bgWidth, bgHeight, bgOffset, bgCSS;
-                
+        imgRatio, bgImg, bgWidth, bgHeight, bgOffset, bgCSS, addlMarkup;
+
         // Extend the settings with those the user has provided
         if(options && typeof options == "object") $.extend(settings, options);
-        
+
         // Just in case the user passed in a function without options
         if(options && typeof options == "function") callback = options;
-    
+
         // Initialize
         $(document).ready(_init);
-  
+
         // For chaining
         return this;
-    
+
         function _init() {
             // Prepend image, wrapped in a DIV, with some positioning and zIndex voodoo
             if(src) {
                 var img;
-                
+
                 // If this is the first time that backstretch is being called
                 if(container.length == 0) {
                     container = $("<div />").attr("id", "backstretch")
+                                            .append(settings.addlMarkup)
                                             .css({left: 0, top: 0, position: "fixed", overflow: "hidden", zIndex: -999999, margin: 0, padding: 0, height: "100%", width: "100%"});
                 } else {
                     // Prepare to delete any old images
                     container.find("img").addClass("deleteable");
                 }
-                
+
                 img = $("<img />").css({position: "absolute", display: "none", margin: 0, padding: 0, border: "none", zIndex: -999999})
-                                  .bind("load", function(e) {                                          
+                                  .bind("load", function(e) {
                                       var self = $(this),
                                           imgWidth, imgHeight;
-                                          
+
                                       self.css({width: "auto", height: "auto"});
                                       imgWidth = this.width || $(e.target).width();
                                       imgHeight = this.height || $(e.target).height();
@@ -67,30 +68,30 @@
                                               if(typeof callback == "function") callback();
                                           });
                                       });
-                                      
+
                                   })
                                   .appendTo(container);
-                 
+
                 // Append the container to the body, if it's not already there
                 if($("body #backstretch").length == 0) {
                     $("body").append(container);
                 }
-                
+
                 // Attach the settings
                 container.data("settings", settings);
-                    
+
                 img.attr("src", src); // Hack for IE img onload event
                 // Adjust the background size when the window is resized or orientation has changed (iOS)
                 $(window).resize(_adjustBG);
             }
         }
-            
+
         function _adjustBG(fn) {
             try {
                 bgCSS = {left: 0, top: 0}
                 bgWidth = rootElement.width();
                 bgHeight = bgWidth / imgRatio;
-                
+
                 // Make adjustments based on image ratio
                 // Note: Offset code provided by Peter Baker (http://ptrbkr.com/). Thanks, Peter!
                 if(bgHeight >= rootElement.height()) {
@@ -103,16 +104,16 @@
                     if(settings.centeredX) $.extend(bgCSS, {left: "-" + bgOffset + "px"});
                 }
 
-                $("#backstretch, #backstretch img:not(.deleteable)").width( bgWidth ).height( bgHeight )
+                $("#backstretch, #backstretch >*:not(.deleteable)").width( bgWidth ).height( bgHeight )
                                                    .filter("img").css(bgCSS);
             } catch(err) {
                 // IE7 seems to trigger _adjustBG before the image is loaded.
                 // This try/catch block is a hack to let it fail gracefully.
             }
-      
+
             // Executed the passed in function, if necessary
             if (typeof fn == "function") fn();
         }
     };
-  
+
 })(jQuery);
